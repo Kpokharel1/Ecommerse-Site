82c4aa70805b6965b8f95c7e5bf18ceeb5d13efa
Keshan1122 <krishnapokharel.2022@gmail.com>
2022-03-29 19:02:32 +0545

    Footer section and website frontpage created

--- js/script.js
+++ js/script.js
@@ -12,3 +12,33 @@ document.addEventListener("DOMContentLoaded", function(){
         } 
     });
   }); 
+  // Increment decrement button
+  let counter = 1;
+
+function increment() {
+  counter++;
+}
+
+function decrement() {
+  counter--;
+}
+
+function get() {
+  return counter;
+}
+
+const inc = document.getElementById("increment");
+const input = document.getElementById("input");
+const dec = document.getElementById("decrement");
+
+inc.addEventListener("click", () => {
+  increment();
+  input.value = get();
+});
+
+dec.addEventListener("click", () => {
+  if (input.value > 0) {
+    decrement();
+  }
+  input.value = get();
+});
